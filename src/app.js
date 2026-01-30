5e38d5f914d5e639b26e01ea3e013cc2d7394041
adit-ya15 <aditya262701@gmail.com>
2026-01-30 16:34:51 +0530

    Write first request handler

--- src/app.js
+++ src/app.js
@@ -2,6 +2,15 @@ const express = require("express");
 
 const app = express();
 
+app.use("/test",(req,res) => {
+    res.send("Test is done");
+});
+
+app.use("/hello",(req,res) => {
+    res.send("Hello to everyone");
+});
+
 app.listen("9999",() => {
     console.log("Server listens on the port 9999");
 });
+
