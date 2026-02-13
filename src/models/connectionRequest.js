bff6ed9c37beb660cb853219e3ab5c7125d77d40
adit-ya15 <aditya262701@gmail.com>
2026-02-13 22:53:02 +0530

    implemeted a validation using a pre method

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -18,6 +18,14 @@ const connectionRequestSchema = new mongoose.Schema({
     }
 },{timestamps : true})
 
+connectionRequestSchema.pre("save",function(next){
+    const connectionRequest = this;
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+        throw new Error("Cannot send request to yourself")
+    }
+    next();
+})
+
 const ConnectionRequest = mongoose.model("ConnectionRequest",connectionRequestSchema);
 
 module.exports = ConnectionRequest;
