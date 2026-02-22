3a587c6d1eefb22fdae63a47bb3b88197e9179b6
adit-ya15 <aditya262701@gmail.com>
2026-02-22 17:25:30 +0530

    Improve the userAuth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -6,7 +6,7 @@ const userAuth = async (req, res, next) => {
         const { token } = req.cookies;
 
         if (!token) {
-            throw new Error("Token is not valid");
+            res.status(401).send("Token is not valid");
         }
 
         const decodedObj = jwt.verify(token, "DevTinder@4648h");
