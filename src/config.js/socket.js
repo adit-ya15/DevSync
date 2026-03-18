161c103d8034fcedeacd881ab993f6e22d4cf7af
adit-ya15 <aditya262701@gmail.com>
2026-03-18 23:29:59 +0530

    Enhance socket authentication and message handling in chat system

--- src/config.js/socket.js
+++ src/config.js/socket.js
@@ -1,10 +1,23 @@
 const socket = require("socket.io");
 const Chat = require("../models/chat");
+const jwt = require("jsonwebtoken");
+const Message = require("../models/message");
 
 const initializeSocket = (server) => {
     const io = socket(server, {
         cors: {
-            origin: "http://localhost:5173"
+            origin: "http://localhost:5173",
+            credentials: true
+        }
+    })
+
+    io.use((socket, next) => {
+        try {
+            const token = socket.handshake.auth.token;
+            const user = jwt.verify(token, process.env.JWT_SECRET);
+            socket.user = user;
+        } catch (error) {
+            next(new Error("Authentication error"))
         }
     })
 
@@ -12,28 +25,28 @@ const initializeSocket = (server) => {
         socket.on("joinChat", (chatId) => {
             socket.join(chatId);
         })
-        socket.on("sendMessage", async ({ firstName, userId, targetUserId, text }) => {
+        socket.on("sendMessage", async ({ chatId, text }) => {
             try {
-                const roomId = [userId, targetUserId].sort().join("A");
+                const userId = socket.user._id;
+                const chat = await Chat.findById(chatId);
 
-                let chat = await Chat.findOne({
-                    participants : {$all : [userId,targetUserId]}
-                })
-
-                if(!chat){
-                    chat = new Chat({
-                        participants : [userId,targetUserId],
-                        messages : []
-                    })
+                if (!chat) {
+                    return;
                 }
 
-                chat.messages.push({
-                    senderId : userId,
+                if (!chat.participants.includes(userId)) return;
+
+                const message = await Message.create({
+                    chatId,
+                    senderId: userId,
                     text
                 })
 
-                await chat.save();
-                io.to(roomId).emit("messageReceived", { firstName, text });
+                await Chat.findByIdAndUpdate(chatId, {
+                    lastMessage: message._id
+                })
+
+                io.to(chatId).emit("messageReceived", message);
             } catch (error) {
                 console.log(error.message)
             }
@@ -41,5 +54,37 @@ const initializeSocket = (server) => {
         socket.on("disconnect", () => { })
     })
 }
+
+
+socket.on("typing", (chatId) => {
+    socket.to(chatId).emit("typing", {
+        userId: socket.user._id
+    });
+});
+
+
+socket.on("stopTyping", (chatId) => {
+    socket.to(chatId).emit("stopTyping", {
+        userId: socket.user._id
+    });
+});
+
+socket.on("markSeen", async ({ chatId }) => {
+    const userId = socket.user._id;
+
+    await Message.updateMany(
+        {
+            chatId,
+            readBy: { $ne: userId }
+        },
+        {
+            $addToSet: { readBy: userId }
+        }
+    );
+
+    io.to(chatId).emit("messagesSeen", {
+        userId
+    });
+});
 
 module.exports = initializeSocket;
--- src/models/message.js
+++ src/models/message.js
@@ -1,31 +1,34 @@
 const mongoose = require("mongoose")
 
 const messageSchema = new mongoose.Schema({
-    chatId : {
-        type : mongoose.Schema.Types.ObjectId,
-        required : true,
-        ref : "Chat",
-        index : true,
+    chatId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Chat",
+        index: true,
     },
-    senderId : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "User",
-        required : true
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
     },
-    text : {
-        type : String,
-        required : true
+    text: {
+        type: String,
+        required: true
     },
-    isEdited : {
-        type : Boolean,
-        default : false
+    isEdited: {
+        type: Boolean,
+        default: false
     },
-    isDeleted : {
-        type : Boolean,
-        default : false
-    }
-},{timestamps : true})
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+    readBy: [
+        { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+    ]
+}, { timestamps: true })
 
-const Message = mongoose.model("Message",messageSchema);
+const Message = mongoose.model("Message", messageSchema);
 
 module.exports = Message;
